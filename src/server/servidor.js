547842659a2f887998246dafc05646858509efa9
Cristian Alvarez H <alvarezhernandezcritian1@gmail.com>
2019-03-19 11:48:09 -0500

    Create Singleton to Servidor class

---
+++ src/server/servidor.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var http = require('http');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+class Servidor {
+
+    constructor(puerto) {
+        this.puerto = puerto;
+        this.app = express();
+
+        this.app.use( function (req, res, next) {
+            // Permite a cualquier origen conectarse
+            res.setHeader('Access-Control-Allow-Origin', '*');
+        
+            // Los metodos permitidos
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        
+            // Headers permitidas
+            res.setHeader('Access-Control-Allow-Headers', 'Authorization,X-Requested-With,content-type');
+
+            // Pasamos a los siguientes middlewares
+            next();
+        });
+
+        this.app.use( morgan('dev') );// utilizamos morgan para ver los logs del server en su modo dev
+
+        // para soportar json encode bodys en las peticiones
+        this.app.use( bodyParser.json() );
+        this.app.use( bodyParser.urlencoded({ extended: true}) );
+
+        this.server = http.createServer(this.app);// creamos un servidor http manejado por express
+    }
+
+    agregarRutas( rutas ) {
+        rutas.
+        .forEach(element => {
+            
+        });
+    }
+
+}
+
+module.exports = Servidor;
